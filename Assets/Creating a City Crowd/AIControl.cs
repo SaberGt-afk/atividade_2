4b351a00237c33a861190e7e85be458dc05c9664
SaberGt-afk <gtsaber0000@gmail.com>
2024-10-08 11:47:41 +0100

    Creating a City Crowd Part 1

---
+++ Assets/Creating a City Crowd/AIControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+    public GameObject goal; // Objetivo que o AI irá seguir
+    private NavMeshAgent agent; // Referência para o componente NavMeshAgent
+
+    void Start() {
+        // Pega a referência do NavMeshAgent
+        agent = GetComponent<NavMeshAgent>();
+
+        // Verifica se o objetivo foi atribuído
+        if (goal != null) {
+            // Define o destino para o objetivo
+            agent.SetDestination(goal.transform.position);
+        } else {
+            Debug.LogError("O GameObject goal não foi atribuído.");
+        }
+    }
+
+    void Update() {
+        // O código de atualização pode ser adicionado aqui, caso seja necessário
+    }
+}
